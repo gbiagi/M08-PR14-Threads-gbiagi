920f047537d166d3f6e7cc575ae9341d9fc75a4e
geanfrancobiagi <geanfrancobiagi@hotmail.com>
2023-10-23 17:01:12 +0200

    Initial commit

---
+++ app/src/main/java/com/example/m08_pr14_threads_gbiagi/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.m08_pr14_threads_gbiagi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
